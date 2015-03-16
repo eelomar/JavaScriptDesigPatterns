632d5692cc1b2fd007054e220c5077a67cdf1b71
eelomar <eelomar-ubuntu>
2015-03-16 11:21:21 +0000

    First version catClicker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$('#my-elem').click(function(e) {
+var totalCount=0;
+var counterEle= $('#counter');
+$('.catimg').click(function(e) {
   //the element has been clicked... do stuff here
+  totalCount+=1;
+ // console.log(totalCount);
+      counterEle.text(totalCount);
+
 });
