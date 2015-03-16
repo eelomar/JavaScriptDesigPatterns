16a6b0cb2820b24ad6671a0cfdc4c23acd3b4383
eelomar <eelomar-ubuntu>
2015-03-16 21:07:39 +0000

    Array of cats, apppear the cat and then click the cat upload counter

--- js/main.js
+++ js/main.js
@@ -1,30 +1,71 @@
-var totalCountRight=0;
-var totalCountLeft=0;
+var $NameCat= $('#nameCat');
+var $CounterCat= $('#counterCat');
+var $listCats=$('#catsList');
+var $divImage=$('#image_loaded');
 
-
-var CatRightName='Cat in the Right';
-var CatleftName='Cat in the Left';
-
-var $RightName= $('#nameCat_right');
-var $LeftName= $('#nameCat_left');
-
-var $RightCounter= $('#counter_right');
-var $LeftCounter= $('#counter_left');
+var catsNames=['cat1','cat2','cat3','cat4','cat5','cat6'];
+var counterCat=[0,0,0,0,0,0];
+var extensionFile='.jpg';
 
 
 
-$('#catimgrigh').click(function(e) {
+
+$('#image_loaded').click(function(e) {
   //the element has been clicked... do stuff here
-  totalCountRight+=1;
- // console.log(totalCount);
- $RightCounter.text(totalCountRight);
- $RightName.text(CatRightName);
+  var name=$NameCat.text();
+  console.log(name);
+  var id=-1;
+  for(var j=0;j<catsNames.length;j++){
+  	if (name===catsNames[j]){
+  		id=j;
+
+  	}
+  }
+  counterCat[id]=counterCat[id]+1;
+  $CounterCat.text(counterCat[id]);
+
 });
-$('#catimgleft').click(function(e) {
-  //the element has been clicked... do stuff here
-  totalCountLeft+=1;
- // console.log(totalCount);
- $LeftCounter.text(totalCountLeft);
- $LeftName.text(CatleftName);
-});
+
+//load all images
+
+
+
+// Let's loop over the numbers in our array
+for (var i = 0; i < catsNames.length; i++) {
+
+    // This is the number we're on...
+    var cat = catsNames[i];
+    counterCat[i];
+    // We're creating a DOM element for the number
+    var elem = document.createElement('option');
+    elem.textContent = cat;
+   // elem.value(cat);
+   var urlImage='images/'+cat+''+extensionFile;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(name,ima) {
+    	return function() {
+    		var id=-1;
+    		for(var j=0;j<catsNames.length;j++){
+    			if (name===catsNames[j]){
+    				id=j;
+    			}
+    		}
+
+    		$divImage.attr('src', ima);
+    		$NameCat.text(name);
+
+    		if(id==-1){    
+    			$CounterCat.text('No counter found');
+    		}else 
+    			$CounterCat.text(counterCat[id]);
+
+    };
+
+
+})(cat,urlImage));
+
+    $listCats.append(elem);
+
+};
 
