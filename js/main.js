06d15d6163f946ca090b60c8b86f4d4e5c326080
eelomar <eelomar-ubuntu>
2015-03-16 12:08:53 +0000

    Modify to have 2 cats

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-var totalCount=0;
-var counterEle= $('#counter');
-$('.catimg').click(function(e) {
-  //the element has been clicked... do stuff here
-  totalCount+=1;
- // console.log(totalCount);
-      counterEle.text(totalCount);
+var totalCountRight=0;
+var totalCountLeft=0;
 
+
+var CatRightName='Cat in the Right';
+var CatleftName='Cat in the Left';
+
+var $RightName= $('#nameCat_right');
+var $LeftName= $('#nameCat_left');
+
+var $RightCounter= $('#counter_right');
+var $LeftCounter= $('#counter_left');
+
+
+
+$('#catimgrigh').click(function(e) {
+  //the element has been clicked... do stuff here
+  totalCountRight+=1;
+ // console.log(totalCount);
+ $RightCounter.text(totalCountRight);
+ $RightName.text(CatRightName);
 });
+$('#catimgleft').click(function(e) {
+  //the element has been clicked... do stuff here
+  totalCountLeft+=1;
+ // console.log(totalCount);
+ $LeftCounter.text(totalCountLeft);
+ $LeftName.text(CatleftName);
+});
+
