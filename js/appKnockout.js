0c8e5fef19afebd4cd7d7fc11b1f8db09814e328
eelomar <eelomar-ubuntu>
2015-03-25 14:50:50 +0000

    Add list of cats and change cat

--- js/appKnockout.js
+++ js/appKnockout.js
@@ -1,14 +1,30 @@
-var ViewModel=function(){
-	this.ClickCount=ko.observable(0);
-	this.name=ko.observable('feo');
-	this.imgSrc=ko.observable('images/cat0.jpg');
-	this.imgAttribution=ko.observable('pero que pollo');
-	this.nickNames=ko.observableArray(['feito','patito','pelele']);
+var initialization=[{
+	name:'cat feo',
+	ClickCount:0,
+	imgSrc:'images/cat0.jpg',
+	imgAttribution:'pero que pollo',
+	nickNames:['feito','patito','pelele']
+},{
+	name:'Pedro',
+	ClickCount:0,
+	imgSrc:'images/cat1.jpg',
+	imgAttribution:'pero que pollo',
+	nickNames:['peta','oulare','muy tecnico']
+},{
+	name:'mago',
+	ClickCount:0,
+	imgSrc:'images/cat2.jpg',
+	imgAttribution:'pero que pollo',
+	nickNames:['123','567','999999999']
+}];
 
-	this.incrementCounter=function(){
-		this.ClickCount(this.ClickCount()+1);
-	};
 
+var Cat=function(data){
+	this.ClickCount=ko.observable(data.ClickCount);
+	this.name=ko.observable(data.name);
+	this.imgSrc=ko.observable(data.imgSrc);
+	this.imgAttribution=ko.observable(data.imgAttribution);
+	this.nickNames=ko.observableArray(data.nickNames);	
 	this.title=ko.computed(function(){
 		var title;
 		var clicks=this.ClickCount();
@@ -25,6 +41,27 @@ var ViewModel=function(){
 		else title='Ninja';
 		return title;
 	},this);
-
 };
+
+
+var ViewModel=function(){
+	var self=this;
+
+	this.catList=ko.observableArray([]);
+	initialization.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+
+	this.currentCat=ko.observable(this.catList()[0]);
+
+	this.incrementCounter=function(){
+		self.currentCat().ClickCount(self.currentCat().ClickCount()+1);
+	};
+
+//	this.setCat=function(thisCat){
+//		self.currentCat(thisCat);
+//	}
+};
+
+
 ko.applyBindings(new ViewModel());
